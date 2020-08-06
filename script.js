0cef94fb393669382cdf3bbb65663a4d3c751d81
Jared <jaredarodriguez@gmail.com>
2020-08-06 09:12:48 -0400

    fetch call returns json

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('hello world')
+// Get Quote From API 
+async function getQuote() {
+    const proxyURL = 'https://cors-anywhere.herokuapp.com/'
+    const apiURL = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try {
+        const response = await fetch(proxyURL + apiURL); 
+        const data = await response.json(); 
+        console.log(data); 
+    } catch (error) {
+        console.log('something went wrong', error);
+    }
+}
+
+// On Load 
+getQuote(); 
