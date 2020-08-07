dbdeeb6912c8e6e12024a0646ef2559f675b9d0a
Jared <jaredarodriguez@gmail.com>
2020-08-07 19:07:57 -0400

    fetch and render quote api

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+
 // Get Quote From API 
 async function getQuote() {
-    const proxyURL = 'https://cors-anywhere.herokuapp.com/'
+    const proxyURL = 'https://cors-anywhere.herokuapp.com/';
     const apiURL = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
         const response = await fetch(proxyURL + apiURL); 
         const data = await response.json(); 
-        console.log(data); 
+        // if author is blank add unknown 
+        if (data.quoteAuthor === '') {
+            authorText.innerText = 'Unknown'
+        } else {
+            authorText.innerText = data.quoteAuthor
+        }
+        // reduce font size for long quotes 
+        if (data.quoteText.length > 120) {
+            quoteText.classList.add('long-quote');
+        } else {
+            quoteText.classList.remove('long-quote');
+        }
+        console.log(data);
+        quoteText.innerText = data.quoteText; 
     } catch (error) {
-        console.log('something went wrong', error);
+        console.log(error);
+        // getQuote(); 
+ 
     }
 }
 
